3183bb58b801eb92fbaeb6f8389b6267dc5fae1a
Jinhjlog <dndn2615@gmail.com>
2024-10-05 18:35:07 +0900

    user 도메인 유닛테스트 코드 수정
    - 프로퍼티 타입 변경 영향

--- src/modules/user/domain/user.spec.ts
+++ src/modules/user/domain/user.spec.ts
@@ -1,14 +1,17 @@
 import { Email } from './email';
+import { Password } from './password';
+import { Phone } from './phone';
 import { User } from './user';
+import { Username } from './username';
 
 describe('User', () => {
   describe('create', () => {
     it('유저 객체를 생성합니다.', () => {
       // given
       const email = Email.create({} as any) as any;
-      const phone = '010-1234-5678';
-      const username = 'jin';
-      const password = 'Test!1234';
+      const phone = Phone.create('010-1234-1234') as any;
+      const username = Username.create('name') as any;
+      const password = Password.create('test!123') as any;
 
       // when
       const user = new User({
